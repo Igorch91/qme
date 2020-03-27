cc199e4ba19dc609e7cb708f3248491a22878931
Igorch91 <igorchesnov@mail.ru>
2020-03-27 21:53:59 +0300

    Baza

--- qme/settings.py
+++ qme/settings.py
@@ -126,8 +126,8 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/qmedata/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
 
 LOGIN_REDIRECT_URL = '/qmedata/'
